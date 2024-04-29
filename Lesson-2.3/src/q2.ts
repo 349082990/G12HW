6f560e07cf9c178549425c793ceb766be5bbb793
zhengvincent <vincentz2006@gmail.com>
2024-04-29 18:45:18 -0400

    q1 cow class

--- Lesson-2.3/src/q2.ts
+++ Lesson-2.3/src/q2.ts
@@ -1,41 +1,125 @@
 class BulkTank {
-  private _capacity: number = 0;
-  private _amount: number = 0;
+  private _tankCapacity: number = 0;
+  private _tankAmount: number = 0;
 
-  constructor(capacity: number = 2000) {
-    this.capacity = capacity;
+  constructor(tankCapacity: number = 2000) {
+    this.tankCapacity = tankCapacity;
   }
 
-  public get capacity(): number {
-    return this.capacity;
+  public get tankCapacity(): number {
+    return this.tankCapacity;
   }
 
-  public set capacity(value: number) {
+  public set tankCapacity(value: number) {
     if (value > 0) {
-      this.capacity = 0;
+      this.tankCapacity = 0;
     } else {
       throw new Error("You cannot input a negative number! Please try again. ");
     }
   }
 
   public addtoTank(addAmount: number): void {
-    if (this._amount + addAmount <= this._capacity) {
-      this._amount + addAmount;
+    if (this._tankAmount + addAmount <= this._tankCapacity) {
+      this._tankAmount + addAmount;
     } else {
-      this._amount = this._capacity;
+      this._tankAmount = this._tankCapacity;
     }
   }
 
   public getFromTank(getAmount: number): void {
-    if (this._amount - getAmount <= this.capacity) {
-      this._amount - getAmount;
+    if (this._tankAmount - getAmount <= this.tankCapacity) {
+      this._tankAmount - getAmount;
     } else {
-      this._amount = 0;
+      this._tankAmount = 0;
     }
+  }
+
+  public getCapacity(): string {
+    return `${this._tankAmount} / ${this._tankCapacity}`;
   }
 }
 
-class Cow {}
+class Cow {
+  private _names: string;
+  private _udderAmount: number = 0;
+  private _isMilkable: boolean = false;
+  readonly udderCapacity: number = Math.floor(Math.random() * 26) + 15; // (max - min + 1) + min
+  constructor(names: string | null = null) {
+    const NAMES = [
+      "Anu",
+      "Arpa",
+      "Essi",
+      "Heluna",
+      "Hely",
+      "Hento",
+      "Hilke",
+      "Hilsu",
+      "Hymy",
+      "Ihq",
+      "Ilme",
+      "Ilo",
+      "Jaana",
+      "Jami",
+      "Jatta",
+      "Laku",
+      "Liekki",
+      "Mainikki",
+      "Mella",
+      "Mimmi",
+      "Naatti",
+      "Nina",
+      "Nyytti",
+      "Papu",
+      "Pullukka",
+      "Pulu",
+      "Rima",
+      "Soma",
+      "Sylkki",
+      "Valpu",
+      "Virpi",
+    ];
+    if (NAMES) {
+      this._names = names;
+    } else {
+      this._names = NAMES[Math.floor(Math.random() * names.length)];
+    }
+  }
+
+  public getCapacity(): string {
+    return `${this._names} ${this._udderAmount}/${this.udderCapacity}`;
+  }
+
+  public checkCapacity(): void {
+    if (this._udderAmount >= this.udderCapacity) {
+      this._isMilkable = true;
+    } else {
+      this._isMilkable = false;
+    }
+  }
+
+  public hourPast(): void {
+    const hourlyRefill = Math.floor(Math.random() * 2.3) + 0.7;
+    if (this._udderAmount + hourlyRefill <= this.udderCapacity) {
+      this._udderAmount += hourlyRefill;
+    } else {
+      this._udderAmount = this.udderCapacity;
+    }
+  }
+
+  public milk(): number {
+    this.checkCapacity();
+
+    if (!this._isMilkable) {
+      throw new Error(
+        "The cow isn't ready to be milked! It's udder is empty. "
+      );
+    }
+
+    const AMOUNT_EXTRACTED = this._udderAmount;
+    this._udderAmount = 0;
+    return AMOUNT_EXTRACTED;
+  }
+}
 
 class MilkingRobots {}
 
